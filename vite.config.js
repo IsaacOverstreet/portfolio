956615cf5f4b215bc95f1ef9b0bafc931a5fc2f1
Isaac <morakinyorisaac@yahoo.com>
2025-01-25 01:17:27 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-site"
+  base: "/portfolio-site",
+  build: {
+    chunkSizeWarningLimit: 1600
+}
 })
