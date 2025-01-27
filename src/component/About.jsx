12d18514c155f4ce4264bfae8ab83527f4e4882e
Isaac <morakinyorisaac@yahoo.com>
2025-01-27 13:20:38 +0100

    first commit

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -10,12 +10,12 @@ return(
             <div className="about-text">
                 <h1>Get to know me</h1>
                 <p>
-                    Hi my name is <b>Morakinyo Isaac</b> i am from Lagos Nigeria.
-                    I am <b>Web Developer</b> currently seeking for internship to apply my skills in a professional setting, collaborate with experience developer and gaining hands-on experience building innovative web solutions.<br/><br/>
-                    I love creating original project with beautiful design for frontend and develop effective solutions for backend.<br/>
+                    Hi my name is <b>Morakinyo Isaac</b>from Lagos Nigeria.
+                    I am a <b>Web Developer</b> currently seeking an internship to apply my skills in a professional setting, collaborate with experience developers, gaining hands-on experience and building innovative web solutions.<br/><br/>
+                    I love creating original project with beautiful design for the frontend and developing effective solutions for the backend.<br/>
                     I am open to collaborations or working where i can contribute and grow. <br/><br/>
+                    In addition to coding, i also love working on computer hardware.<br/>
                     I have my links in my <b>footer</b>, so you can connect with me.
-                    In addition to coding i also love working on computer hardwares.<br/>
                 </p>
             </div>
 
